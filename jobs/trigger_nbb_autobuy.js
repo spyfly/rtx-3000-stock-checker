e029461cbd02f574d6ac591c5763c45197bfe4ac
Sebastian Heiden <seb.heiden@gmail.com>
2021-01-31 20:07:36 +0100

    Fixing AutoBuy Trigger (and adding debug)

--- jobs/trigger_nbb_autobuy.js
+++ jobs/trigger_nbb_autobuy.js
@@ -10,25 +10,25 @@ Object.keys(config.autobuy.cards).forEach(card => {
     db.get(card + " NBB", function (err, nbbStatus) {
         db.get(card + " NV", function (err, nvStatus) {
             //Checking if card is in stock
-            //if (nbbStatus != "out_of_stock" || nvStatus != "out_of_stock") {
-            const urls = config.autobuy.cards[card];
-            //Call autobuy URLs
-            urls.forEach(url => {
-                http.get(url, (res) => {
-                    res.setEncoding('utf8');
-                    let rawData = '';
-                    res.on('data', (chunk) => { rawData += chunk; });
-                    res.on('end', () => {
-                        try {
-                            const data = JSON.parse(rawData);
-                            console.log(data);
-                        } catch (e) {
-                            console.error(e.message);
-                        }
+            if (nbbStatus != "out_of_stock" || nvStatus != "out_of_stock" || config.autobuy.debug == true) {
+                const urls = config.autobuy.cards[card];
+                //Call autobuy URLs
+                urls.forEach(url => {
+                    http.get(url, (res) => {
+                        res.setEncoding('utf8');
+                        let rawData = '';
+                        res.on('data', (chunk) => { rawData += chunk; });
+                        res.on('end', () => {
+                            try {
+                                const data = JSON.parse(rawData);
+                                console.log(data);
+                            } catch (e) {
+                                console.error(e.message);
+                            }
+                        });
                     });
                 });
-            });
-            //}
+            }
         });
     });
 });
