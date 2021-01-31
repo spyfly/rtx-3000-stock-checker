705dd024579f9b601aa9c81fb78d3688859d8d3e
Sebastian Heiden <seb.heiden@gmail.com>
2021-01-31 19:26:56 +0100

    Only notify when status changes (to prevent spam)

--- jobs/check_nbb.js
+++ jobs/check_nbb.js
@@ -7,6 +7,9 @@ const puppeteer = require('puppeteer');
 const bot = new TelegramBot(config.services.telegram.token);
 const chat_id = config.services.telegram.chat_id;
 
+const level = require('level-party')
+var db = level('./status', { valueEncoding: 'json' })
+
 config.nbb.cards.forEach(async card => {
     await checkNbb(card);
 });
@@ -26,27 +29,37 @@ async function checkNbb(card) {
     await page.screenshot({ path: 'debug_' + card.toLowerCase().replace(" ", "+") + '.png' });
 
     const data = await page.content();
+    var message, status;
+
     console.log(productName);
 
     //Checking Page Contents
     if (data.includes("client has been blocked by bot protection.")) {
-        const message = "NBB Bot blocked by bot protection!";
-        console.log("> " + message);
-        bot.sendMessage(chat_id, message);
+        message = "NBB Bot blocked by bot protection!";
+        status = "blocked_by_bot_protection";
     } else if (data.includes(productName)) {
         //console.log("Successfully fetched product page!")
         if (data.includes("Dieses Produkt ist leider ausverkauft.") || data.includes("Leider ist dieser Artikel nicht mehr verfügbar.")) {
-            console.log("> " + "Still not in stock!")
+            message = "RTX " + card + " out of Stock on NBB";
+            status = "out_of_stock";
         } else {
-            const message = "Buy RTX " + card + " on NBB: " + storeUrl;
-            console.log("> " + message)
-            bot.sendMessage(chat_id, message);
+            message = "Buy RTX " + card + " on NBB: " + storeUrl;
+            status = "in_stock"
         }
     } else {
-        const message = "Couldn't fetch NBB product page, maybe new bot protection?";
-        console.log("> " + message)
-        bot.sendMessage(chat_id, message);
+        message = "Couldn't fetch NBB product page, maybe new bot protection?";
+        status = "fetch_failure"
     }
+
+    console.log("> " + message)
+
+    const db_key = card + " NBB";
+    db.get(db_key, function (err, oldStatus) {
+        if (oldStatus != status) {
+            bot.sendMessage(chat_id, message);
+            db.put(db_key, status, function (err) { });
+        }
+    });
 
     await browser.close();
     console.log("------------------------------------------------------------------")
--- jobs/check_nvidia.js
+++ jobs/check_nvidia.js
@@ -6,6 +6,9 @@ const config = require('../config.json');
 const bot = new TelegramBot(config.services.telegram.token);
 const chat_id = config.services.telegram.chat_id;
 
+const level = require('level-party')
+var db = level('./status', { valueEncoding: 'json' })
+
 var data = "";
 
 const nvShopUrl = 'https://www.nvidia.com/de-de/shop/geforce/';
@@ -37,12 +40,26 @@ req.on("end", () => {
         products.forEach(function (product) {
             if (product.isFounderEdition) {
                 console.log(product.displayName);
+                var status, message;
+                const card = product.displayName.replace("Nvidia RTX ", "");
+
                 if (product.prdStatus != 'out_of_stock' || product.purchaseOption != '' || product.isOffer != false) {
+                    status = "in_stock";
                     console.log("> Is in stock!")
-                    bot.sendMessage(chat_id, product.displayName + " is " + product.prdStatus + " at " + nvShopUrl);
                 } else {
+                    status = "out_of_stock";
                     console.log("> Still out of stock. | Stock Status: " + product.prdStatus)
                 }
+
+                message = product.displayName + " is " + product.prdStatus + " at " + nvShopUrl;
+
+                const db_key = card + " NV";
+                db.get(db_key, function (err, oldStatus) {
+                    if (oldStatus != status) {
+                        bot.sendMessage(chat_id, message);
+                        db.put(db_key, status, function (err) { });
+                    }
+                });
                 console.log("------------------------------------------------------------------")
             }
         });
