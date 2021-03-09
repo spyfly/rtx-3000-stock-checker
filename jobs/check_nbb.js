56259e4e5c6687ff5088db861d08e6cc70639fd8
Sebastian Heiden <seb.heiden@gmail.com>
2021-03-09 22:08:50 +0100

    Added Proxy Support for checking NBB.com

--- jobs/check_nbb.js
+++ jobs/check_nbb.js
@@ -10,25 +10,55 @@ const chat_id = config.services.telegram.chat_id;
 const level = require('level-party')
 var db = level('./status', { valueEncoding: 'json' })
 
+const UserAgent = require('user-agents');
+
 config.nbb.cards.forEach(async card => {
     await checkNbb(card);
 });
 
 async function checkNbb(card) {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1280, height: 720 })
+    var browserDetails = {
+        userAgent: config.browser.user_agent,
+        viewport: {
+            width: 1280,
+            height: 720
+        },
+        cookies: []
+    };
+    var puppeteer_args = {};
+    var proxy;
 
-    //Messing with the User Agent to circumvent Bot Prevention
-    await page.setUserAgent(config.browser.user_agent);
+    //Using a proxy
+    if (config.nbb.proxies) {
+        proxy = await getRandomProxy();
+        browserDetails = await getBrowserDetails(proxy);
+        puppeteer_args.userDataDir = '/tmp/rtx-3000-stock-checker/' + proxy.replace(/\./g, "-").replace(/\:/g, "_");
+        puppeteer_args.args = ['--proxy-server=http://' + proxy];
+    }
+
+    const browser = await puppeteer.launch(puppeteer_args);
+    const page = await browser.newPage();
+
+    if (config.nbb.proxies) {
+        await page.setCookie(...browserDetails.cookies);
+        console.log("Set cookies");
+    }
+
+    //Messing with the User Agent and Viewport to circumvent Bot Prevention
+    await page.setCacheEnabled(true);
+    await page.setViewport(browserDetails.viewport)
+    await page.setUserAgent(browserDetails.userAgent);
+
 
     const storeUrl = 'https://www.notebooksbilliger.de/nvidia+geforce+rtx+' + card.toLowerCase().replace(" ", "+") + '+founders+edition';
     const productName = "NVIDIA GeForce RTX " + card + " Founders Edition";
 
-    await page.goto(storeUrl);
-    await page.screenshot({ path: 'debug_' + card.toLowerCase().replace(" ", "+") + '.png' });
+    await page.goto(storeUrl, { waitUntil: 'load', timeout: 0 });
+
+    console.log("Page loaded")
 
     const data = await page.content();
+
     var message, status;
 
     console.log(productName);
@@ -61,6 +91,59 @@ async function checkNbb(card) {
         }
     });
 
+    //Click away cookies banner if cookies haven't been set yet
+    if (browserDetails.cookies.length == 0) {
+        await page.waitForSelector("#uc-btn-deny-banner");
+        await page.click("#uc-btn-deny-banner");
+    }
+
+    await page.screenshot({ path: 'debug_' + card.toLowerCase().replace(" ", "+") + '.png' });
+
+    if (config.nbb.proxies) {
+        await updateCookies(proxy, await page.cookies());
+    }
     await browser.close();
     console.log("------------------------------------------------------------------")
+}
+
+async function getBrowserDetails(proxy) {
+    var rawDetails;
+    try {
+        rawDetails = await db.get(proxy);
+        console.log("Found old details, parsing")
+        return JSON.parse(rawDetails);
+    } catch {
+        console.log("Generating new Browser Details for " + proxy)
+        const userAgent = new UserAgent({ deviceCategory: 'desktop' });
+        details = {
+            userAgent: userAgent.userAgent,
+            viewport: {
+                height: userAgent.viewportHeight,
+                width: userAgent.viewportWidth
+            },
+            cookies: []
+        }
+        db.put(proxy, JSON.stringify(details))
+        return details;
+    }
+}
+
+async function updateCookies(proxy, cookies) {
+    var rawDetails;
+    try {
+        rawDetails = await db.get(proxy);
+        var updatedDetails = JSON.parse(rawDetails);
+        updatedDetails.cookies = cookies;
+        await db.put(proxy, JSON.stringify(updatedDetails));
+        console.log("Updated cookies");
+    } catch {
+        console.log("Failed storing cookies");
+    }
+}
+
+async function getRandomProxy() {
+    const proxyCount = config.proxies.length
+    const proxyId = Math.floor(Math.random() * proxyCount);
+    const proxy = config.proxies[proxyId];
+    return proxy;
 }
