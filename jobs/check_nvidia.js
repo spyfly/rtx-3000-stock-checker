10ac828b14bdcaf4abcb8f746432bf67212bcf47
Sebastian Heiden <seb.heiden@gmail.com>
2021-03-10 01:47:40 +0100

    Fix Nvidia Check

--- jobs/check_nvidia.js
+++ jobs/check_nvidia.js
@@ -5,34 +5,53 @@ const config = require('../config.json');
 const bot = new TelegramBot(config.services.telegram.token);
 const chat_id = config.services.telegram.chat_id;
 
+const puppeteer = require('puppeteer');
+
+const imposter = require('../libs/imposter.js');
+
 const level = require('level-party')
 var db = level('./status', { valueEncoding: 'json' })
-
-const { curly } = require('node-libcurl');
 
 const nvShopUrl = 'https://www.nvidia.com/de-de/shop/geforce/';
 
 async function main() {
-    var curl_config = {
-        httpHeader: [
-            'User-Agent: ' + config.browser.user_agent
-        ]
+    var browserDetails = {
+        userAgent: config.browser.user_agent,
+        viewport: {
+            width: 1280,
+            height: 720
+        },
+        cookies: []
     };
+    var puppeteer_args = {};
+    var proxy;
 
-    //Get Random Proxy
+    //Using a proxy
     if (config.nvidia.proxies) {
-        const imposter = require('../libs/imposter.js');
-
-        const proxy = await imposter.getRandomProxy();
-        const browserDetails = await imposter.getBrowserDetails(proxy);
-        curl_config.httpHeader = ['User-Agent: ' + browserDetails.userAgent]
-        curl_config.proxy = proxy;
+        proxy = await imposter.getRandomProxy();
+        browserDetails = await imposter.getBrowserDetails(proxy);
+        puppeteer_args.userDataDir = '/tmp/rtx-3000-stock-checker/' + proxy.replace(/\./g, "-").replace(/\:/g, "_");
+        puppeteer_args.args = ['--proxy-server=http://' + proxy];
     }
 
-    const { statusCode, data, headers } = await curly.get("https://api.nvidia.partners/edge/product/search?page=1&limit=9&locale=de-de", curl_config);
+    const browser = await puppeteer.launch(puppeteer_args);
+    const page = await browser.newPage();
+
+    //Messing with the User Agent and Viewport to circumvent Bot Prevention
+    await page.setCacheEnabled(true);
+    await page.setViewport(browserDetails.viewport)
+    await page.setUserAgent(browserDetails.userAgent);
+
+    await page.goto("https://api.nvidia.partners/edge/product/search?page=1&limit=9&locale=de-de", { waitUntil: 'load', timeout: 0 });
+    var content = await page.content()
+    content = content.replace('<html><head></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">', "");
+    content = content.replace('</pre></body></html>', '')
+
+    await page.close();
+    await browser.close();
 
     try {
-        const json = data;
+        const json = JSON.parse(content);
         const products = json.searchedProducts.productDetails;
         products.push(json.searchedProducts.featuredProduct);
         products.forEach(function (product) {
@@ -66,4 +85,5 @@ async function main() {
         bot.sendMessage(chat_id, "An error occurred fetching Nvidias page, cards may be available: " + nvShopUrl);
     }
 }
+
 main();
