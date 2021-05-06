476eae3e9a7073eb8ffcf19208e595a7c5141fe3
Sebastian Heiden <seb.heiden@gmail.com>
2021-05-06 02:15:17 +0200

    Override viewport height for debug

--- jobs/check_ceconomy.js
+++ jobs/check_ceconomy.js
@@ -147,6 +147,7 @@ async function getProducts(store, override = false) {
             proxy = "default"
             console.log("All proxies blacklisted, using no proxy!")
         }
+        browser_context.viewport.height = 10000;
     }
 
     //const browser = await chromium.launchPersistentContext('/tmp/rtx-3000-stock-checker/' + proxy.replace(/\./g, "-").replace(/\:/g, "_"), browser_context);
